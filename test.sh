3282dd8f81e2042e1501b3d13cc3a7e9cc40fa16
ravi datt <ravidatt@gmail.com>
2020-01-04 19:44:05 -0500

    first commit

--- test.sh
+++ test.sh
@@ -7,3 +7,8 @@ echo 'hello there. this will run in jenkins on check in'
 echo 'this will run in jenkins on git commit'
 date
 pwd
+
+echo 'hello there. this will run in jenkins on check in'
+echo 'this will run in jenkins on git commit'
+date
+pwd
