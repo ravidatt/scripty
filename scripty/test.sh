a7ed1743dbc81d8a964300458c0ce379aa11d4ec
ravi datt <ravidatt@gmail.com>
2020-01-04 19:45:08 -0500

    first commit

--- scripty/test.sh
+++ scripty/test.sh
@@ -1,1 +1,14 @@
 echo 'hello there. this will run in jenkins on check in'
+echo 'this will run in jenkins on git commit'
+date
+pwd
+
+echo 'hello there. this will run in jenkins on check in'
+echo 'this will run in jenkins on git commit'
+date
+pwd
+
+echo 'hello there. this will run in jenkins on check in'
+echo 'this will run in jenkins on git commit'
+date
+pwd
